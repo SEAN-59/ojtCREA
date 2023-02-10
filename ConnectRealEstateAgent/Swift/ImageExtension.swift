05ff69e753e126699eef24c6a1ff6a9fd918dc61
Company <ksg3452@gmail.com>
2023-02-10 15:21:10 +0900

    Add ImageAsset

---
+++ ConnectRealEstateAgent/Swift/ImageExtension.swift
@@ -0,0 +1,14 @@
+//
+//  ImageExtension.swift
+//  ConnectRealEstateAgent
+//
+//  Created by TAnine on 2023/02/10.
+//
+
+import UIKit
+
+extension UIImage {
+    class var iconGoogle: UIImage? { return UIImage(named: "GoogleIcon")}
+    class var iconNaver: UIImage? { return UIImage(named: "NaverIcon")}
+    class var iconKakao: UIImage? { return UIImage(named: "KakaoIcon")}
+}
